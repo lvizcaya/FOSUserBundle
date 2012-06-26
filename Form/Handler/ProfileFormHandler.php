3114c32e0a2aaa422a8631107e7a3f31914021f8
Leonardo Vizcaya <lvizcaya@Inspiron-1420.(none)>
2012-06-25 22:28:10 -0430

    Actualizo el manejador para poder modificar la edición de perfil

--- Form/Handler/ProfileFormHandler.php
+++ Form/Handler/ProfileFormHandler.php
@@ -34,7 +34,7 @@ class ProfileFormHandler
     public function process(UserInterface $user)
     {
         $this->form->setData(($user));
-
+        
         if ('POST' === $this->request->getMethod()) {
             $this->form->bindRequest($this->request);
 
